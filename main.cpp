b2dcd339a1b4ac464eb1a21e1596b78f9d02d0a7
juhanbin <gksqls0509@naver.com>
2023-11-15 10:48:25 +0900

    cout 사용법

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << 10 <<std::endl;
+    std::cout << 10U << std::endl;
+    std::cout << 10.5F << std::endl;
+    std::cout << 10.5 << std::endl;
+    std::cout << 3 + 4 << std::endl;
     return 0;
 }
