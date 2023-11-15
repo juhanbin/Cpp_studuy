100e184ec8161a9dee733047a4c1db5bb82ff990
juhanbin <gksqls0509@naver.com>
2023-11-15 10:54:10 +0900

    문자열 조합 출력

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+#include <string>
 #include <iostream>
 
-int main() {
-    std::cout << 10 <<std::endl;
-    std::cout << 10U << std::endl;
-    std::cout << 10.5F << std::endl;
-    std::cout << 10.5 << std::endl;
-    std::cout << 3 + 4 << std::endl;
+int main()
+{
+    std::string strData = "Test string";
+    std::cout << "Sample string" << std::endl;
+    std::cout << strData << std::endl;
+
+    strData = "New string";
+    std::cout << strData <<std::endl;
+
+    std::cout << "저는 " << 20 << "살" << "입니다." << std::endl;
     return 0;
 }
