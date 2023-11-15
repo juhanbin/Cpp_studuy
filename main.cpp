7aceaa4d9467a3c4cd1d9f6443e0781e722b6f76
juhanbin <gksqls0509@naver.com>
2023-11-15 13:06:43 +0900

    auto 예약어 사용

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
-#include <string>
-#include <cstdio>
 #include <iostream>
 
 int main()
 {
-    int nAge;
-    std::cout << "나이를 입력하세요."<< std::endl;
-    std::cin >> nAge;
+    int a =10;
+    int b(a);
+    auto c(b);
 
-    char szJob[32];
-    std::cout << "직업을 입력하세요." << std::endl;
-    std::cin >> szJob;
+    std::cout << a + b + c << std::endl;
 
-    std::string strName;
-    std::cout << "이름을 입력하세요." << std::endl;
-    std::cin >> strName;
-
-    std::cout << "당신의 이름은 " << strName << "이고, " << "나이는 " << nAge << "살이며 "
-    << "직업은 " << szJob << "입니다." << std::endl;
     return 0;
 }
