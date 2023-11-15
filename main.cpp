97f4fea752a1191d05176eaad6df4579afeace85
juhanbin <gksqls0509@naver.com>
2023-11-15 11:04:21 +0900

    std::cin사용법

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <string>
+#include <cstdio>
 #include <iostream>
 
 int main()
 {
-    std::string strData = "Test string";
-    std::cout << "Sample string" << std::endl;
-    std::cout << strData << std::endl;
+    int nAge;
+    std::cout << "나이를 입력하세요."<< std::endl;
+    std::cin >> nAge;
 
-    strData = "New string";
-    std::cout << strData <<std::endl;
+    char szJob[32];
+    std::cout << "직업을 입력하세요." << std::endl;
+    std::cin >> szJob;
 
-    std::cout << "저는 " << 20 << "살" << "입니다." << std::endl;
+    std::string strName;
+    std::cout << "이름을 입력하세요." << std::endl;
+    std::cin >> strName;
+
+    std::cout << "당신의 이름은 " << strName << "이고, " << "나이는 " << nAge << "살이며 "
+    << "직업은 " << szJob << "입니다." << std::endl;
     return 0;
 }
