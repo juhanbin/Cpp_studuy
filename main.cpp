06a58abac194e2737c515abdcdbd09d45cf79872
juhanbin <gksqls0509@naver.com>
2023-11-15 13:19:16 +0900

    배열 형태의 객체 생성

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    int *pData = new int;
+    int *arr = new int[5];
+    for(int i = 0; i < 5; ++i)
+        arr[i] = (i + 1) * 10;
 
-    int *pNewData = new int(10);
+    for(int i = 0; i < 5; ++i)
+        cout << arr[i] <<endl;
 
-    *pData = 5;
-    std::cout << *pData << std::endl;
-    std::cout << *pNewData << std::endl;
-
-    delete pData;
-    delete pNewData;
+    delete[] arr;
     return 0;
 }
