e1731429c98f41b70834248071746f0c27c35c28
juhanbin <gksqls0509@naver.com>
2023-11-18 16:08:41 +0900

    매개변수가 두 개일 때의 디폴트 값

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
-int TestFunc(int nParam = 10)
+//int TestFunc(int =10);
+int TestFunc(int nParam1 = 1, int nParam2 = 2)
 {
-    return nParam;
+    return nParam1 * nParam2;
 }
 
 int main()
 {
-    std::cout << TestFunc() << std::endl;
-    std::cout << TestFunc(20) << std::endl;
+    std::cout << TestFunc(10) << std::endl;
+    std::cout << TestFunc(10, 5)<< std::endl;
     return 0;
 }
