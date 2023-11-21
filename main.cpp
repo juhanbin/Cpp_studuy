ced405036320527621677bdf2b0aabbd465157b3
juhanbin <gksqls0509@naver.com>
2023-11-21 11:05:39 +0900

    범위 검색의 확장

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int nData(20);
-
 int main(int argc)
 {
-    int nData(10);
+    int nInput = 0;
+    cout <<"11 이상의 정수를 입력하세요" << endl;
+    cin >>nInput;
 
-    cout << nData <<endl;
-    cout << argc <<endl;
-
+    if(nInput > 10)
+    {
+        cout <<nInput <<endl;
+    }
+    else
+        cout << "Error" << endl;
     return 0;
 }
