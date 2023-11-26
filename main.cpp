111e916182987a595f5144d4823185e245047204
juhanbin <gksqls0509@naver.com>
2023-11-26 15:57:09 +0900

    생성자 함수의 역할

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ class CTest
 public:
     CTest()
     {
+        cout << "CTest() : 생성자 함수" << endl;
         m_nData = 10;
     }
 
@@ -18,9 +19,12 @@ public:
 };
 int main()
 {
+    cout << "main() 함수 시작" << endl;
+
     CTest t;
     t.PrintData();
 
+    cout << "main() 함수 끝" << endl;
 
     return 0;
 }
