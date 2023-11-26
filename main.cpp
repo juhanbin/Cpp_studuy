cc3c3bb2f173b3c5aa7ec4ed034d5c12da09f6cb
juhanbin <gksqls0509@naver.com>
2023-11-26 16:05:23 +0900

    맴버 함수 선언과 정의를 분리

--- main.cpp
+++ main.cpp
@@ -6,25 +6,20 @@ class CTest
 public:
     CTest()
     {
-        cout << "CTest() : 생성자 함수" << endl;
         m_nData = 10;
     }
+    int m_nData;
 
-    int  m_nData;
-
-    void PrintData(void)
-    {
-        cout << m_nData << endl;
-    }
+    void PrintData(void);
 };
+
+void CTest::PrintData()
+{
+    cout << m_nData << endl;
+}
 int main()
 {
-    cout << "main() 함수 시작" << endl;
-
     CTest t;
     t.PrintData();
-
-    cout << "main() 함수 끝" << endl;
-
     return 0;
 }
