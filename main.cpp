b4743b163403f920947664c2b0dc824ae9ea5dcc
juhanbin <gksqls0509@naver.com>
2023-11-26 16:11:33 +0900

    생성자 초기화 목록을 이용한 멤버 변수 초기화

--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@ class CTest
 {
 public:
     CTest()
-    {
-        m_nData = 10;
-    }
-    int m_nData;
+    : m_nData1(10),m_nData2(20)
+    { }
+
+    int m_nData1;
+    int m_nData2;
 
     void PrintData(void);
 };
 
 void CTest::PrintData()
 {
-    cout << m_nData << endl;
+    cout << m_nData1 << endl;
+    cout << m_nData2 << endl;
 }
 int main()
 {
