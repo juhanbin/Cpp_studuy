c74b43434b178ba0a20423704b7d95083010f3f9
juhanbin <gksqls0509@naver.com>
2023-12-13 21:15:57 +0900

    디폴트 생성자의 생략

--- main.cpp
+++ main.cpp
@@ -3,20 +3,23 @@ using namespace std;
 
 class CTest
 {
+    int m_nData;
 public:
-    CTest()
+    CTest(int nParam) : m_nData(nParam)
     {
         cout << "CTest::CTest()" << endl;
     }
     ~CTest()
     {
-        cout << "~CTest::CTest()" << endl;
+        cout << "~CTest::CTest()" << m_nData<< endl;
     }
 };
 int main()
 {
     cout << "Begin" << endl;
-    CTest a;
+    CTest a(1);
+    cout << "Before b" << endl;
+    CTest b(2);
     cout << "End" << endl;
 
     return 0;
