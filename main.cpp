2f5987d77f11269ae6d558247a23d8f1d858347f
juhanbin <gksqls0509@naver.com>
2023-12-15 10:11:50 +0900

    다른 생성자를 추가로 부르는 생성자 초기화 함수

--- main.cpp
+++ main.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
 using namespace std;
 
-class CMyData
+class CMyPoint
 {
 public:
-    CMyData(int nParam) : m_nData(nParam) { };
-    CMyData(int x, int y) : m_nData(x + y) { };
+    CMyPoint(int x) {
+        cout << "CMyPoint(int)" << endl;
+        if (x > 100)
+            x = 100;
+        m_x = 100;
+    }
+    CMyPoint(int x, int y)
+            :CMyPoint(x)
+    {
+        cout << "CMyPoint(int, int)" << endl;
 
-    int GetData(void) { return m_nData; }
+        if(y > 200)
+            y = 200;
+            m_y = 200;
+    }
+
+    void Print()
+    {
+        cout << "X:" << m_x << endl;
+        cout << "Y:" << m_y << endl;
+    }
 
 private:
-    int m_nData;
+    int m_x = 0;
+    int m_y = 0;
+
 };
 int main()
 {
-    CMyData a(10);
-    CMyData b(3,4);
+    CMyPoint ptBegin(110);
+    ptBegin.Print();
 
-    cout << a.GetData() << endl;
-    cout << b.GetData() << endl;
+    CMyPoint ptEnd(50,250);
+    ptEnd.Print();
+
     return 0;
 }
