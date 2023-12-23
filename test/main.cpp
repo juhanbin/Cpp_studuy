e48381f086c9ace565f9666d1a80812960566f66
juhanbin <gksqls0509@naver.com>
2023-12-23 15:54:40 +0900

    복사 생성자 선언 및 정의

---
+++ test/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class CMyData
+{
+public:
+    CMyData() { cout << "CMyData()"; }
+    CMyData(const CMyData &rhs)
+    // : m_nData(rhs.m_nData)
+    {
+        this->m_nData = rhs.m_nData;
+        cout << "CMyDta(const CMyData &)" << endl;
+    }
+    int GetData(void) const { return m_nData; }
+    void SetData(int nParam) { m_nData = nParam; }
+
+private:
+    int m_nData = 0;
+};
+int main()
+{
+    CMyData a;
+    a.SetData(10);
+
+    CMyData b(a);
+    cout << b.GetData() << endl;
+
+    return 0;
+}
