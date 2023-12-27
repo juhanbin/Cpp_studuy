52c12e09dfd71acb340084751e02e531213c514f
juhanbin <gksqls0509@naver.com>
2023-12-27 18:30:17 +0900

    포인터가 없는 복사 생성자 사용

--- test/main.cpp
+++ test/main.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
+class CMyData
+{
+public:
+    CMyData(){ cout << "CMyData()" << endl; }
 
+    int GetData(void) const { return m_nData; }
+    void SetData(int nParam) { m_nData = nParam; }
+
+private:
+    int m_nData = 0;
+};
 int main()
 {
-    int *pA, *pB;
+    CMyData a;
+    a.SetData(10);
+    CMyData b(a);
+    cout << b.GetData() << endl;
 
-    pA = new int;
-    *pA = 10;
-
-    pB = new int;
-    pB = pA;
-
-    cout << *pA << endl;
-    cout << *pB << endl;
-
-    delete pA;
-    delete pB;
     return 0;
 }
