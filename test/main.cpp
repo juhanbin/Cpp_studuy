bc0f12b841f51145763782a89b533fc02f263f46
juhanbin <gksqls0509@naver.com>
2023-12-27 16:26:52 +0900

    얕은 복사의 문제점

--- test/main.cpp
+++ test/main.cpp
@@ -2,37 +2,21 @@
 
 using namespace std;
 
-class CTestData
-{
-public:
-    CTestData(int  nParam) : m_nData(nParam)
-    {
-        cout << "CTestData(int)" <<endl;
-    }
-    CTestData(const CTestData &rhs) : m_nData(rhs.m_nData)
-    {
-        cout << "CTestData(const CTestData &)" << endl;
-    }
-    int GetData() const { return m_nData; }
-    void SetData(int nParam) { m_nData = nParam; }
-
-private:
-    int m_nData = 0;
-};
-
-void TestFunc(CTestData param)
-{
-    cout << "TestFunc()" << endl;
-    param.SetData(20);
-}
 
 int main()
 {
-    cout << "begin" << endl;
-    CTestData a(10);
-    TestFunc(a);
+    int *pA, *pB;
 
-    cout << "a: " << a.GetData() << endl;
-    cout << "end" << endl;
+    pA = new int;
+    *pA = 10;
+
+    pB = new int;
+    pB = pA;
+
+    cout << *pA << endl;
+    cout << *pB << endl;
+
+    delete pA;
+    delete pB;
     return 0;
 }
