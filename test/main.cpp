45922074aa1b6ad5809b33d4c4fed6561b9dc45f
juhanbin <gksqls0509@naver.com>
2024-01-03 23:01:41 +0900

    다중 정의의 모호성

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void TestFunc(int &rParam)
-{
-    cout << "TestFunc(int &)" << endl;
-}
 void TestFunc(int &&rParam)
 {
     cout << "TestFunc(int &&)" << endl;
+}
+void TestFunc(int rParam)
+{
+    cout << "TestFunc(int)" << endl;
 }
 int main()
 {
