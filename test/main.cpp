2bbddc1cebcc838c7ec3c005abeacec87945f200
juhanbin <gksqls0509@naver.com>
2024-01-16 21:27:14 +0900

    메서드 재정의 기본

--- test/main.cpp
+++ test/main.cpp
@@ -4,29 +4,31 @@ using namespace std;
 class CMyData
 {
 public:
-    int GetData() { return m_nData; }
+    int GetData(){ return m_nData; }
     void SetData(int nParam) { m_nData = nParam; }
+
 private:
     int m_nData = 0;
 };
-
 class CMyDataEx : public CMyData
 {
 public:
     void SetData(int nParam)
     {
         if(nParam < 0)
-            CMyData::SetData(10);
+            CMyData::SetData(0);
         if(nParam > 10)
             CMyData::SetData(10);
     }
 };
 int main(int argc, char* argv[])
 {
-    CMyDataEx a;
-    CMyData &rData = a;
-    rData.SetData(15);
-    cout << rData.GetData() << endl;
+    CMyData a;
+    a.SetData(-10);
+    cout << a.GetData() << endl;
+    CMyDataEx b;
+    b.SetData(15);
+    cout << b.GetData() << endl;
 
     return 0;
 }
