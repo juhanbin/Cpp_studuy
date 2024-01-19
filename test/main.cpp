95d5d3bbc80dca7b6c42f0099f28d10892e6a439
juhanbin <gksqls0509@naver.com>
2024-01-20 02:16:28 +0900

    늦은 바인딩

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void TestFunc(int nParam) { }
+void TestFunc1(int nParam) { }
+void TestFunc2(int nParam) { }
 
 int main(int argc, char* argv[])
 {
-    TestFunc(10);
+    int nInput = 0;
+    cin >> nInput;
+    void(*pfTest)(int) = NULL;
+
+    if(nInput > 10)
+        pfTest = TestFunc1;
+    else
+        pfTest = TestFunc2;
+    pfTest(10);
     return 0;
 }
