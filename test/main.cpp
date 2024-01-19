bb0db659f3eb70a05ad60fb0b37abb6b94b8b6a5
juhanbin <gksqls0509@naver.com>
2024-01-20 02:12:45 +0900

    이른 바인딩

--- test/main.cpp
+++ test/main.cpp
@@ -1,38 +1,10 @@
 #include <iostream>
 using namespace std;
 
-class CMyData
-{
-public:
-    CMyData()
-    {
-        cout << "CMyDAta()" << endl;
-    }
-    virtual ~CMyData() { }
-    virtual void TestFunc1() { }
-    virtual void TestFunc2() { }
-};
-
-class CMyDataEx : public CMyData
-{
-public:
-    CMyDataEx()
-    {
-        cout << "CMyDataEx()" << endl;
-    }
-    virtual ~CMyDataEx() { }
-    virtual void TestFunc1() { }
-    virtual void TestFunc2()
-    {
-        cout << "TestFunc2()" << endl;
-    }
-};
+void TestFunc(int nParam) { }
 
 int main(int argc, char* argv[])
 {
-    CMyData *pData = new CMyDataEx;
-    pData->TestFunc2();
-    delete pData;
-
+    TestFunc(10);
     return 0;
 }
