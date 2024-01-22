5a235d4f00c61518e9cb88b677909dc0df5cdd3b
juhanbin <gksqls0509@naver.com>
2024-01-22 21:44:06 +0900

    인터페이스

--- test/main.cpp
+++ test/main.cpp
@@ -1,32 +1,50 @@
 #include <iostream>
 using namespace std;
 
-class CMyInterface
+class CMyObject
 {
 public:
-    CMyInterface()
+    CMyObject() { }
+    virtual ~CMyObject() { }
+    virtual int GetDeviceID() = 0;
+
+protected:
+    int m_nDeviceID;
+};
+
+void PrintID(CMyObject *pObj)
+{
+    cout << "Device ID: " << pObj->GetDeviceID() << endl;
+}
+
+class CMyTV : public CMyObject
+{
+public:
+    CMyTV(int nID) { m_nDeviceID = nID; }
+    virtual int GetDeviceID()
     {
-        cout << "CMyInterface()" << endl;
+        cout << "CMyTV::GetDeviceID()" << endl;
+        return m_nDeviceID;
     }
-    virtual int GetData() const = 0;
-    virtual void SetData(int nParam) = 0;
 };
-class CMyData : public CMyInterface
+
+class CMyPhone : public CMyObject
 {
 public:
-    CMyData() { cout << "CMyData()" << endl; }
-
-    virtual int GetData() const { return m_nData; }
-    virtual void SetData(int nParam) { m_nData = nParam; }
-private:
-    int m_nData = 0;
+    CMyPhone(int nID) { m_nDeviceID = nID; }
+    virtual int GetDeviceID()
+    {
+        cout << "CMyPhone::GetDeviceID()" << endl;
+        return m_nDeviceID;
+    }
 };
-
 int main(int argc, char* argv[])
 {
-    CMyData a;
-    a.SetData(5);
-    cout << a.GetData() << endl;
+    CMyTV a(5);
+    CMyPhone b(10);
+
+    ::PrintID(&a);
+    ::PrintID(&b);
 
     return 0;
 }
