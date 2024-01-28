3ef9bd6d068da47e29aa497bc2046449f4afd52d
juhanbin <gksqls0509@naver.com>
2024-01-28 22:27:11 +0900

    가상 상속 적용 전

--- test/main.cpp
+++ test/main.cpp
@@ -1,49 +1,32 @@
 #include <iostream>
 using namespace std;
 
-class CMyImage
+class CMyObject
 {
 public:
-    CMyImage(int nHeight, int nWidth)
-    : m_nHeight(nHeight),m_nWidth(nWidth)
-    {
-        cout << "CMyImage(int, int)" << endl;
-    }
-    int GetHeight() const { return m_nHeight;}
-    int GetWidth() const { return m_nWidth; }
-
-protected:
-    int m_nHeight;
-    int m_nWidth;
+    CMyObject() { cout << "CMyObject()" << endl; }
+    virtual ~CMyObject() { }
 };
 
-class CMyShape
+class CMyImage : public CMyObject
 {
 public:
-    CMyShape(int nType) : m_nType(nType)
-    {
-        cout << "CMyShape(int)" << endl;
-    }
+    CMyImage() { cout << "CMyImage(int, int)"<< endl; }
+};
 
-    int GetType() const { return m_nType; }
-
-protected:
-    int m_nType;
+class CMyShape : public CMyObject
+{
+public:
+    CMyShape() { cout << "CMyShape(int)" << endl; }
 };
 
 class CMyPicture : public CMyImage, public CMyShape
 {
 public:
-    CMyPicture() : CMyImage(200, 120), CMyShape(1)
-    {
-        cout << "CMyPicture()" << endl;
-    }
+    CMyPicture() { cout << "CMyPicture()" << endl; }
 };
 int main(int argc, char* argv[])
 {
     CMyPicture a;
-    cout << "Width: " << a.GetWidth() << endl;
-    cout << "Height: "<< a.GetHeight() << endl;
-    cout << "Type: " << a.GetType() << endl;
     return 0;
 }
