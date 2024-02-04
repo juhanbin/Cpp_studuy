f0d3d5c92802e528172aa0ecb3c13ab724dcbd89
juhanbin <gksqls0509@naver.com>
2024-02-04 17:34:47 +0900

    멤버 선언과 정의 분리

--- test/main.cpp
+++ test/main.cpp
@@ -2,92 +2,35 @@
 using namespace std;
 
 template<typename T>
-class CMyArray
+class CTest
 {
 public:
-    explicit CMyArray(int nSize) : m_nSize(nSize)
-    {
-        m_pData = new T[nSize];
-    }
+    CTest();
+    T TestFunc();
 
-    ~CMyArray() { delete[] m_pData; }
-
-    CMyArray(const CMyArray &rhs)
-    {
-        m_pData = new T[rhs.m_nSize];
-        memcpy(m_pData, rhs.m_pData, sizeof(T)* rhs.m_nSize);
-        m_nSize = rhs.m_nSize;
-    }
-
-    CMyArray& operator=(const CMyArray &rhs)
-    {
-        if(this == &rhs)
-            return *this;
-
-        delete m_pData;
-        m_pData = new T[rhs.m_nSize];
-        memcpy(m_pData, rhs.m_pData, sizeof(T)* rhs.m_nSize);
-        m_nSize = rhs.m_nSize;
-
-        return *this;
-    }
-
-    CMyArray(CMyArray &&rhs)
-    {
-        operator = (rhs);
-    }
-
-    CMyArray& operator=(const CMyArray &&rhs)
-    {
-        m_pData = rhs.m_pData;
-        m_nSize = rhs.m_nSize;
-        rhs.m_pData = nullptr;
-        rhs.m_nSize = 0;
-    }
-
-    T& operator[](int nIndex)
-    {
-        if(nIndex < 0 || nIndex >= m_nSize)
-        {
-            cout << "ERROR: 배열의 경계를 벗어난 접근입니다." << endl;
-            exit(1);
-        }
-        return m_pData[nIndex];
-    }
-
-    T& operator[](int nIndex) const
-    {
-        return operator[](nIndex);
-    }
-
-    int GetSize() { return m_nSize; }
-
-private:
-    T *m_pData = nullptr;
-    int m_nSize = 0;
+protected:
+    static T m_Data;
 };
+
+template<typename T>
+CTest<T>::CTest()
+{
+
+}
+
+template<typename T>
+T CTest<T>::TestFunc()
+{
+    return m_Data;
+}
+
+template<typename T>
+T CTest<T>::m_Data = 15;
 
 int main(int argc, char* argv[])
 {
-    CMyArray<int> arr(5);
-
-    arr[0] = 10;
-    arr[1] = 20;
-    arr[2] = 30;
-    arr[3] = 40;
-    arr[4] = 50;
-
-    for(int i = 0; i < 5; ++i)
-        cout << arr[i] << ' ';
-
-    cout << endl;
-
-    CMyArray<int> arr2(3);
-    arr2 = arr;
-    for(int i = 0; i < 5; ++i)
-        cout << arr2[i] << ' ';
-
-    cout << endl;
+    CTest<double> a;
+    cout << a.TestFunc() << endl;
 
     return 0;
 }
